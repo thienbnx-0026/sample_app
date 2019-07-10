4fe9168fa85df07a61a2fb08f4932658c4915377
Thien <menkisede2@gmail.com>
2019-07-10 09:31:05 +0700

    chap_8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
+
+  protect_from_forgery with: :exception
+  include SessionsHelper
+
   private
   def default_url_options
     {locale: I18n.locale}
